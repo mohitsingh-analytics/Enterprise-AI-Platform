31d1b72823645ccfd5244f667aa67c54347ce348
mohit singh <mohitsingh.bvp@gmail.com>
2026-08-04 00:26:13 +0530

    feat adding reqeust lifecycle workflow

--- app/middleware.py
+++ app/middleware.py
@@ -6,6 +6,7 @@ class RequestMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
 
         try:
+            request.state.request_id = str(uuid.uuid4())
             response = await call_next(request)
             return response
 
